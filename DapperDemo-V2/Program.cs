28ccc164b617a7c3fb7cf8533d9b2e0a1932620e
johndward01 <johndward01@gmail.com>
2021-10-25 09:49:54 -0500

    Add project files.

---
+++ DapperDemo-V2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperDemo_V2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
